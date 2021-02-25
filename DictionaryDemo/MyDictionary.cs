2f8c7418360d8ce22189fef8034cf39cbd65099e
Ahmet Enes Polat <ahmetenes3458@gmail.com>
2021-02-25 23:40:32 +0300

    Add project files.

---
+++ DictionaryDemo/MyDictionary.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DictionaryDemo
+{
+    class MyDictionary<K,V>
+    {
+        K[] number;
+        V[] names;
+
+        public MyDictionary()
+        {
+            number = new K[0];
+            names = new V[0];
+        }
+        public void Add(K KeyItem, V ValueItem)
+        {
+            K[] tempArray = number;
+            number = new K[number.Length + 1];
+            V[] tempArray2 = names;
+            names = new V[names.Length + 1];
+
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                number[i] = tempArray[i];
+            }
+            number[number.Length - 1] = KeyItem;
+
+            
+            for (int i = 0; i < tempArray2.Length; i++)
+            {
+                names[i] = tempArray2[i];
+            }
+            names[names.Length - 1] = ValueItem;
+
+        }
+
+        public int count
+        {
+            get { return number.Length; }
+
+        }
+    }
+}
---
+++ DictionaryDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DictionaryDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> students = new MyDictionary<int, string>();
+            students.Add(7, "Kürşad Kırkçeri");
+            students.Add(207,"Metehan Oğuzbeyi");
+            students.Add(1453,"Fatih Mehmet");
+            students.Add(1923, "Mustafa Kemal");
+            students.Add(1299, "Osman Gazibey");
+            students.Add(1336, "Emir Timur");
+            Console.WriteLine(students.count);
+        }
+    }
+}
